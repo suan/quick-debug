2b9d6c97e9e8cfb3fc5efe5901e45261cf3fe643
Suan-Aik Yeo <yeosuanaik@gmail.com>
2013-01-02 12:27:34 -0600

    add ability to enable/disable debug statements

--- lib/quick-debug.rb
+++ lib/quick-debug.rb
@@ -3,16 +3,29 @@ require "#{File.dirname(Pathname.new(__FILE__).realpath)}/quick-debug/version"
 
 class D
   @@logpath = '/tmp/quick-debug.txt'
+  @@active = {:bg => true, :lg => true}
+
+  def self.disable where
+    locations = where == :all ? [:bg, :lg] : where
+    locations.each{ |location| @@active[location] = false }
+  end
+
+  def self.enable where
+    locations = where == :all ? [:bg, :lg] : where
+    locations.each{ |location| @@active[location] = true }
+  end
 
   def self.logpath= path
     @@logpath = path
   end
 
   def self.bg(command = nil, &block)
+    return if not @@active[:bg]
     puts eval_inspect(caller.first, command, &block)
   end
 
   def self.lg(command = nil, &block)
+    return if not @@active[:lg]
     caller_method = caller.first
     timestamp = Time.now.strftime("%a %H:%M:%S")
     File.open(@@logpath, 'a+') do |f|
